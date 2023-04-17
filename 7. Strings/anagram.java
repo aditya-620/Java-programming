563342b9c30f022aa3597cab5dc723f58a3c45dd
Aditya <adityarastogi620@gmail.com>
2023-04-17 23:18:24 +0530

    new programs

---
+++ 7. Strings/anagram.java
@@ -0,0 +1,44 @@
+/*Two strings, a and b, are called anagrams if they contain all the same characters in the same frequencies.
+ For this challenge, the test is not case-sensitive.
+ For example, the anagrams of CAT are CAT, ACT, tac, TCA, aTC, and CtA. */
+
+public class anagram {
+    static Boolean isAnagram(String a, String b) {
+        a=a.toLowerCase();
+        b=b.toLowerCase();
+        
+        if(a.length()!=b.length()){
+            return false;
+        }
+        int arr[] = new int[26];
+        for(int i=0; i<a.length();i++){
+            int index = a.charAt(i)-'a';
+            arr[index]++;
+        }
+        for(int i=0; i<b.length(); i++){
+            int index = b.charAt(i)-'a';
+            arr[index]--;
+        }
+        for(int i=0; i<26; i++){
+            if(arr[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+    
+        
+        String a = "anagram";
+        String b = "margana"; 
+        
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
+
+//If you like my code do give stars:-)
+//Keep coding!!
+//Thankyou;-)
