ffaee0417bbf7be8a945ebb58c77e65e83e74a33
Aditya <adityarastogi620@gmail.com>
2023-04-19 23:44:38 +0530

    new programs

--- 4. Array/array4.java
+++ 4. Array/array4.java
@@ -1,4 +1,28 @@
 //program to find all possible pairs of array
 public class array4 {
-    
+    public static void reverse(int numbers[]){
+        int first = 0, last = numbers.length-1;
+
+        while(first<last){
+            //swap
+            int temp = numbers[last];
+            numbers[last] = numbers[first];
+            numbers[first] = temp;
+
+            first++;
+            last--;
+        }
+    }
+    public static void main(String args[]){
+        int numbers[] = {2, 4, 6, 8, 10};
+        reverse(numbers);
+        for(int i = 0; i<numbers.length; i++){
+            System.out.print(numbers[i] +" ");
+        }
+        System.out.println();
+    }
 }
+
+//If you like my code do give stars:-)
+//Keep coding!!
+//Thankyou;-)
--- 7. Strings/regex.java
+++ 7. Strings/regex.java
@@ -20,8 +20,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Scanner;
 public class regex {
-
-
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         while(in.hasNext()){
@@ -34,6 +32,19 @@ public class regex {
 
 //Write your code here
 class MyRegex{
-    String num = "([01]?\\d{1,2}|2[0-4]\\d|25[0-5])";
+    /*
+    [01]?\\d{1,2}   matches numbers 0-199
+    2[0-4]\\d       matches numbers 200-249
+    25[0-5]         matches numbers 250-255
+     */
+
+    String num = "([01]?\\d{1,2}|2[0-4]\\d|25[0-5])";     //regex for IP address
     String pattern = num +"." +num +"." +num +"." +num;
 }
+
+
+//For more info visit: https://www.hackerrank.com/challenges/java-regex/problem?isFullScreen=true
+
+//If you like my code do give stars:-)
+//Keep coding!!
+//Thankyou;-)
