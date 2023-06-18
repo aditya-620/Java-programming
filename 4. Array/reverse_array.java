5cdf980c808af8db00ea6b33948e46dae7711792
Aditya <adityarastogi620@gmail.com>
2023-06-18 07:44:04 +0530

    file modified

--- 4. Array/reverse_array.java
+++ 4. Array/reverse_array.java
@@ -1,3 +1,26 @@
 public class reverse_array {
-    
+    public static void reverse(int numbers[]){
+        int first=0, last=numbers.length-1;
+
+        while(first<last){
+            //swap
+            int temp = numbers[last];
+            numbers[last]=numbers[first];
+            numbers[first]=temp;
+
+            first++;
+            last--;
+        }
+    }
+    public static void main(String arggs[]){
+        int numbers[]={2,4,6,8,10};
+        reverse(numbers);
+        for(int i=0; i<numbers.length;i++){
+            System.out.print(numbers[i] +" ");
+        }
+    }
 }
+
+//If you like my code do give stars:-)
+//Keep coding!!
+//Thankyou;-)
