3e0a7c87eae8d83426dfcdc4508a5dbe3256a9e0
Aditya <adityarastogi620@gmail.com>
2023-07-09 20:01:07 +0530

    occurence

--- 10. Recursion/occurence.java
+++ 10. Recursion/occurence.java
@@ -1,7 +1,7 @@
-//find the first and last occurance of an element
+//find the first and last occurence of an element
 public class occurence {
-    //function to find the first occurance
-    public static int firstOccurance(int arr[], int key, int i){
+    //function to find the first occurence
+    public static int firstOccurence(int arr[], int key, int i){
         //base case
         if(i == arr.length){
             return -1;
@@ -10,16 +10,16 @@ public class occurence {
         if(arr[i] == key){
             return i;
         }
-        return firstOccurance(arr, key, i+1);
+        return firstOccurence(arr, key, i+1);
     }
 
-    //function to find the last occurance
-    public static int lastOccurance(int arr[], int key, int i){
+    //function to find the last occurence
+    public static int lastOccurence(int arr[], int key, int i){
         //base case
         if(i == arr.length){
             return -1;
         }
-        int isFound = lastOccurance(arr, key , i+1);
+        int isFound = lastOccurence(arr, key , i+1);
         if(isFound == -1 && arr[i] == key){
             return i;
         }
@@ -27,11 +27,11 @@ public class occurence {
     }
     public static void main(String args[]){
         int arr[] = {8,3,6,9,5,10,2,5,3};
-        System.out.println(firstOccurance(arr, 5 , 0));
-        System.out.println(lastOccurance(arr, 5 , 0));
+        System.out.println(firstOccurence(arr, 5 , 0));
+        System.out.println(lastOccurence(arr, 5 , 0));
     }
 }
 
 //If you like my code do give stars:-)
 //Keep coding!!
-//Thankyou;-)
+//Thankyou;-) 
