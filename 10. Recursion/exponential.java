b90bf2c63fbd84d7a0e64922eee8dea6aa297b0c
Aditya <adityarastogi620@gmail.com>
2023-07-11 09:12:02 +0530

    exponent

--- 10. Recursion/exponential.java
+++ 10. Recursion/exponential.java
@@ -1,12 +1,30 @@
+//program to print the value of x^n
 public class exponential {
-    public static int power(int x, int n){
-        if(n == 0){
+    // public static int power(int x, int n){
+    //     if(n == 0){
+    //         return 1;}
+    //     return x * power(x , n-1);          //time complexity: O(n)
+    // }
+
+    //optimized code (Time Complexity: O(logn))
+    public static int optimizedPower(int x, int n){
+        if (n == 0){
             return 1;
         }
-        int value = x * power(x , n-1);
-        return value;
+        int halfPower = optimizedPower(x, n/2);
+        int halfpowersq = halfPower * halfPower;
+
+        //n = odd
+        if(n % 2 != 0){
+            halfpowersq = x * halfpowersq;
+        }
+        return halfpowersq;
     }
     public static void main(String args[]){
-        System.out.println(power(2,10));
+        System.out.println(optimizedPower(2,10));
     }
 }
+
+//If you like my code do give stars:-)
+//Keep coding!!
+//Thankyou;-) 
