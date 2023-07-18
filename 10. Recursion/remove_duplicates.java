eea40c24de69e0d1fe040ff8e5ea380285852a03
Aditya <adityarastogi620@gmail.com>
2023-07-18 07:56:09 +0530

    remove duplicates

---
+++ 10. Recursion/remove_duplicates.java
@@ -0,0 +1,23 @@
+public class remove_duplicates {
+    public static void removeDuplicates(String str, int idx, StringBuilder newstr, boolean map[]){
+        //base case
+        if(idx == str.length()){
+            System.out.println(newstr);
+            return;
+        }
+
+        char currChar = str.charAt(idx);
+        if(map[currChar - 'a'] == true){
+            //duplicate
+            removeDuplicates(str, idx+1, newstr, map);
+        }else{
+            map[currChar - 'a'] = true;
+            removeDuplicates(str, idx+1, newstr.append(currChar), map);
+        }
+    }
+    public static void main(String args[]){
+        String str = "adityarastogi";
+        removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
+    }
+    
+}
