c5bbc87ec29b6c59d1fb8a77d78ecb9f1390f277
Aditya <adityarastogi620@gmail.com>
2023-07-20 21:42:36 +0530

    binary string problem

---
+++ 10. Recursion/binary_string_problem.java
@@ -0,0 +1,22 @@
+public class binary_string_problem {
+    public static void printBinaryString(int n, int lastPlace, String str){
+        //base case
+        if(n == 0){
+            System.out.println(str);
+            return;
+        }
+        // if(lastPlace == 0){
+        //     printBinaryString(n-1, 0, str+"0");
+        //     printBinaryString(n-1, 1, str+"1");
+        // }else{
+        //     printBinaryString(n-1, 0, str+"0");
+        // }
+        printBinaryString(n-1, 0, str+"0");
+        if(lastPlace == 0){
+            printBinaryString(n-1, 1, str+"1");
+        }
+    }
+    public static void main(String args[]){
+        printBinaryString(3, 0, "");
+    }
+}
