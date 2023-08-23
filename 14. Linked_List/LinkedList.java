ff0b52daaf0964db4633fd33745b7ac5f6ea0a2f
Aditya <adityarastogi620@gmail.com>
2023-08-23 23:18:11 +0530

    Linked List

--- 14. Linked_List/LinkedList.java
+++ 14. Linked_List/LinkedList.java
@@ -39,13 +39,29 @@ public class LinkedList {
         tail = newNode;
 
     }
-    
+    public void print(){
+        if(head == null){
+            System.out.println("LinkedList is empty");
+            return;
+        }
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data +"->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
 
     public static void main(String args[]){
         LinkedList ll = new LinkedList();
+        ll.print();
         ll.addFirst(2);
+        ll.print();
         ll.addFirst(1);
+        ll.print();
         ll.addLast(3);
+        ll.print();
         ll.addLast(4);
+        ll.print();
     }
 }
