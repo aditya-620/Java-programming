2f869091e8cfd4da9fd746a91caf92b7e60cb334
Aditya <adityarastogi620@gmail.com>
2023-09-30 19:24:38 +0530

    reverse Doubly LL

---
+++ 14. Linked_List/reverse_DLL.java
@@ -0,0 +1,72 @@
+import org.w3c.dom.Node;
+
+public class reverse_DLL {
+    public class Node{
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    //ADD NODES AT FIRST
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;      
+    }
+
+    //PRINT
+    public void print(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data +"<->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    //REVERSE 
+    public void reverse(){
+        Node curr = head;
+        Node prev = null;
+        Node next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            curr.prev = next;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+    
+    public static void main(String args[]){
+        reverse_DLL dll = new reverse_DLL();
+        dll.addFirst(5);
+        dll.addFirst(4);
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+
+        dll.print();
+
+        dll.reverse();
+
+        dll.print();
+    }
+}
