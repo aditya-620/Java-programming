518f1b2e1f70f653a4c82d68c0dcfe980f47585d
Aditya <adityarastogi620@gmail.com>
2023-10-05 01:45:45 +0530

    interleave

---
+++ 16. Queues/interleave.java
@@ -0,0 +1,43 @@
+//interleave 2 halves of a queue(even lngth)
+import java.util.*;
+import java.util.LinkedList;
+public class interleave {
+    public static void interLeave(Queue<Integer> q){
+        Queue<Integer> firstHalf = new LinkedList<>();
+
+        int size = q.size();
+
+        for(int i=0; i<size/2; i++){
+            firstHalf.add(q.remove());
+        }
+        while(!firstHalf.isEmpty()){
+            q.add(firstHalf.remove());
+            q.add(q.remove());
+        }
+    }
+    public static void main(String args[]){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(9);
+        q.add(10);  
+        
+        interLeave(q);
+
+        //print
+        while(!q.isEmpty()){
+            System.out.print(q.remove() +" ");
+        }
+        System.out.println();
+    }
+}
+
+//If you like my code do give stars:-)
+//Keep coding!!
+//Thankyou;-)
