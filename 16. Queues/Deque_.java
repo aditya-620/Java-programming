975f1ff8df02fe4806130a2feda5fd9dd9270304
Aditya <adityarastogi620@gmail.com>
2023-10-05 02:03:59 +0530

    deque

---
+++ 16. Queues/Deque_.java
@@ -0,0 +1,20 @@
+//opterations on deque using JCF
+import java.util.*;
+import java.util.LinkedList;
+public class Deque_ {
+    public static void main(String args[]){
+        Deque<Integer> deque = new LinkedList<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addLast(3);
+        deque.addLast(4);
+
+        System.out.println(deque);  
+        deque.removeFirst();
+        System.out.println(deque);
+    }
+}
+
+//If you like my code do give stars:-)
+//Keep coding!!
+//Thankyou;-)
