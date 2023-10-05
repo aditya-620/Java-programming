9b019544da5bca9a68af1f327afcada15e4e18c0
Aditya <adityarastogi620@gmail.com>
2023-10-05 23:18:28 +0530

    activity selection

--- 17. Greedy_Algorithm/activity_selection.java
+++ 17. Greedy_Algorithm/activity_selection.java
@@ -1,33 +1,48 @@
 //activity selection problem
+import java.lang.reflect.Array;
 import java.util.*;
 public class activity_selection {
     public static void main(String args[]){
         int start[] = {1, 3, 0, 5, 8, 5};
         int end[] = {2, 4, 6, 7, 9, 9};
 
+        //sorting
+        int activities[][] = new int[start.length][3];
+        for(int i=0; i<start.length; i++){
+            activities[i][0] = i;
+            activities[i][1] = start[i];
+            activities[i][2] = end[i];
+        }
+
+        //lambda function
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
+
+
         //end time basis sorted
         int maxAct = 0;
         ArrayList<Integer> ans = new ArrayList<>();
 
         //1st activity
-        max.Act = 1;
-        and.add(0);
+        maxAct = 1;
+        ans.add(activities[0][0]);
 
-        int lastEnd = end[0];
+        int lastEnd = activities[0][2];
         for(int i=0; i<end.length; i++){
-            if(start[i] >= lastEnd) {
+            if(activities[i][1] >= lastEnd) {
                 //activity select
                 maxAct++;
-                ans.add(i);
-                lastEnd = end[i];
+                ans.add(activities[i][0]);
+                lastEnd = activities[i][2];
             }
         }
         System.out.println("max activities: " +maxAct);
         for(int i=0; i<ans.size(); i++){
-            System.out.println("A" +ans.get(i));
+            System.out.print("A" +ans.get(i) +" ");
         }
     }
 }
+
+//Time Complexity: O(n)
 
 //refer this for more info: https://www.geeksforgeeks.org/activity-selection-problem-greedy-algo-1/
 
