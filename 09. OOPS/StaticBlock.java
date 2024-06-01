f1080781e5b9b4d22ec8b8eb2a20c873acfee0c3
Aditya <adityarastogi620@gmail.com>
2024-06-01 21:32:56 +0530

    static Block

---
+++ 09. OOPS/StaticBlock.java
@@ -0,0 +1,27 @@
+// this is a demo to show initialization of static variables
+public class StaticBlock {
+    static int a = 4;
+    static int b;
+
+    // will only run once, when the first obj is create i.e. when the class is loaded for the first time
+    static {
+        System.out.println("I am in static block");
+        b = a * 5;
+    }
+
+    public static void main(String[] args) {
+        StaticBlock obj = new StaticBlock();
+        System.out.println(StaticBlock.a + " " + StaticBlock.b);
+
+        StaticBlock.b += 3;
+
+        System.out.println(StaticBlock.a + " " + StaticBlock.b);
+
+        StaticBlock obj2 = new StaticBlock();
+        System.out.println(StaticBlock.a + " " + StaticBlock.b);
+    }
+}
+
+//If you like my code do give stars:-)
+//Keep coding!!
+//Thankyou;-)
