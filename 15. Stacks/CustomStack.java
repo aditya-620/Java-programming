1d3fa4e96db8dac26ecaa42bbc2170387391168a
Aditya <adityarastogi620@gmail.com>
2024-09-04 00:25:31 +0530

    Custom Stack

---
+++ 15. Stacks/CustomStack.java
@@ -0,0 +1,74 @@
+
+public class CustomStack {
+    public class StackException extends Exception{
+
+        public StackException(String message) {
+            super(message);
+        }
+    }
+    protected int[] data;
+    private static final int DEFAULT_SIZE = 10;
+
+    int ptr = -1;
+
+    public CustomStack(){
+        this(DEFAULT_SIZE);
+    }
+
+    public CustomStack(int size) {
+        this.data = new int[size];
+    }
+
+    public boolean push(int item) {
+        if (isFull()) {
+            System.out.println("Stack is full!!");
+            return false;
+        }
+        ptr++;
+        data[ptr] = item;
+        return true;
+    }
+
+    public int pop() throws StackException {
+        if (isEmpty()) {
+            throw new StackException("Cannot pop from an empty stack!!");
+        }
+//        int removed = data[ptr];
+//        ptr--;
+//        return removed;
+        return data[ptr--];
+    }
+
+    public int peek() throws StackException {
+        if (isEmpty()) {
+            throw new StackException("Cannot peek from an empty stack!!");
+        }
+        return data[ptr];
+    }
+
+    public boolean isFull() {
+        return ptr == data.length - 1; // ptr is at last index
+    }
+
+    public boolean isEmpty() {
+        return ptr == -1;
+    }
+    public static void main(String[] args) throws StackException{
+        CustomStack stack = new CustomStack(5);
+
+        stack.push(34);
+        stack.push(45);
+        stack.push(2);
+        stack.push(9);
+        stack.push(18);
+        stack.push(89);
+
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
+}
